3f00e5fd9d971151d7cd16b3f7d4c73aa0247660
leandroHigino <59128087+leandroHigino@users.noreply.github.com>
2024-09-07 11:14:59 -0300

    up

--- admin/functions/db_connect.php
+++ admin/functions/db_connect.php
@@ -1,18 +1,16 @@
-<?php 
-    // Mostrar o erro prontado na tela 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
-    
+<?php
+// Mostrar o erro na tela 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-    $mysqli = new mysqli('localhost', 'root', '', 'blog');
 
-    $mysqli->set_charset('utf8mb4');
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-    // printf("Success... %s\n", $mysqli->host_info);
+$mysqli = new mysqli('localhost', 'root', '', 'blog');
 
-?>
+$mysqli->set_charset('utf8mb4');
+
+// printf("Success... %s\n", $mysqli->host_info);
